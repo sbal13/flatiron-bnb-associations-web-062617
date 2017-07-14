5560bed2f262ab4d97579bb4e12145d6875f95a8
sbal13 <sbal13@gmail.com>
2017-07-14 13:58:20 -0400

    minor fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,4 @@ class User < ActiveRecord::Base
 	has_many :reservations, through: :listings
 	has_many :trips, :class_name => 'Reservation', :foreign_key => 'guest_id'
 	has_many :reviews, :foreign_key => 'guest_id'
-	# has_many :reviews, :foreign_key => 'host_id', through: :
 end
